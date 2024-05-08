d32ae3497d6b9d04acc4940d336e671acdf94476
Truong Chi Nhan <nhanbin03@gmail.com>
2024-05-08 12:12:30 +0700

    Update PORT to use environment variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 4001;
+const { PORT } = require('./config');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
