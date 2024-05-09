2fd70b2f6a1e7c6be7bac1f64ce7541b542c653f
Truong Chi Nhan <nhanbin03@gmail.com>
2024-05-09 11:13:05 +0700

    Add user model

---
+++ models/userModel.js
@@ -0,0 +1,51 @@
+const db = require('../db');
+const queryGen = require('../utils/queryGen');
+
+module.exports = {
+    async create(username, password) {
+        const { rows } = await db.query(
+            queryGen.create('users', { username, password })
+        );
+        if (rows.length) {
+            return rows[0];
+        }
+        return null;
+    },
+
+    async findOne(options) {
+        const { rows } = await db.query(
+            queryGen.find('users', options)
+        );
+        if (rows.length) {
+            return rows[0];
+        }
+        return null;
+    },
+
+    async update(data, options) {
+        const { rows } = await db.query(
+            queryGen.update('users', data, options)
+        );
+        if (rows.length) {
+            return rows[0];
+        }
+        return null;
+    },
+
+    async delete(options) {
+        const { rows } = await db.query(
+            queryGen.delete('users', options)
+        );
+        if (rows.length) {
+            return rows[0];
+        }
+        return null;
+    },
+
+    async findAll() {
+        const { rows } = await db.query(
+            queryGen.findAll('users')
+        );
+        return rows;
+    },
+}
